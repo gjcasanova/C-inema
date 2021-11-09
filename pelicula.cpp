cd6b0e0be905c3b99c09598f609c66f6f63cdc88
Guillermo Casanova <guillermo_m_casanova@hotmail.com>
2021-11-09 18:22:57 -0500

    Pelicula class implemented

--- pelicula.cpp
+++ pelicula.cpp
@@ -2,12 +2,23 @@
 
 Pelicula::Pelicula()
 {
+    this->id = -1; // -1 indica que no se le ha definido un id;
+    this->titulo = "";
+    this->duracion = 0.0;
+    this->estreno = false;
+    this->hora = "";
 }
 
 Pelicula::Pelicula(int id, int titulo, float duracion, bool estreno, string hora)
 {
+    this->id = id;
+    this->titulo = titulo;
+    this->duracion = duracion;
+    this->estreno = estreno;
+    this->hora = hora;
 }
 
 void Pelicula::mostrarInfo()
 {
+    cout << "[" << this->hora << "] " << this->titulo << " (" << duracion << ") " << (this->estreno ? "* ESTRENO *" : "") << endl;
 }
